865abd01a9fdb3b19e96ba55463edc1faa9c19b1
Andrew Bruce <andrew@andrewbruce.me>
2023-08-09 12:32:02 +1000

    added anti spam question and updated info to bring it up to date

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,34 @@
 $(document).ready(function () {
+    
+    var num1 = Math.floor(Math.random() * 10)+1;
+    var num2 = Math.floor(Math.random() * 10)+1;
+
+    document.getElementById("validationQuestion").innerHTML = num1 + " + " + num2 + " (anti-spam)"
+
     $("#contact_form").submit(function(e) {
-
-
         var form = $(this);
+        var validationAnswer = document.getElementById("validationAnswer").value;
 
-        $.ajax({
-            type: "POST",
-            url: "https://formsapi.jabwn.com/key/gGN8D0kTtPSOmuqbwcBh",
-            data: form.serialize(), // serializes the form's elements.
-            success: function(data)
-            {
-                ok_message("Email Sent", "Thank you for you email, I will reply as soon as possible.")
-                document.getElementById("name").value = "";
-                document.getElementById("email").value = "";
-                document.getElementById("message").value = "";
-            }
-        });
+        if (num1 + num2 == validationAnswer)
+        {
+            $.ajax({
+                type: "POST",
+                url: "https://formsapi.jabwn.com/key/gGN8D0kTtPSOmuqbwcBh",
+                data: form.serialize(), // serializes the form's elements.
+                success: function(data)
+                {
+                    ok_message("Email Sent", "Thank you for your email, I will reply as soon as possible.")
+                    document.getElementById("name").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("message").value = "";
+                    document.getElementById("validationAnswer").value = "";
+                }
+            });
+        }
+        else 
+        {
+            ok_message("Erorr - Email not sent", "You answered the anti-spam question incorrectly. Please try again, unless you are a robot :-)")
+        }
 
         e.preventDefault(); // avoid to execute the actual submit of the form.
     });
